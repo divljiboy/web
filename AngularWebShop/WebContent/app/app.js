34c026d3dc1cbcb6aa006d7e60d30090ea8567a8
divljiboy <ivan93.ns@hotmail.com>
2016-08-06 12:00:29 +0200

    bla

--- AngularWebShop/WebContent/app/app.js
+++ AngularWebShop/WebContent/app/app.js
@@ -15,4 +15,28 @@ webShop.config(function($routeProvider) {
 
 webShop.config(function($logProvider){
     $logProvider.debugEnabled(true);
+// napravimo modul
+var webShop = angular.module('webShop', ['ui.router']);
+
+webShop.config(function($stateProvider, $urlRouterProvider) {
+	$urlRouterProvider.otherwise('/');
+	
+	$stateProvider
+		.state('products',
+			{
+				//controller: 'productsController',// inace je podeseno ng-controller atributom
+				url: '/',
+				templateUrl: 'partials/products.html'
+			})
+		.state('shoppingCart',
+		{
+			//controller: 'shoppingCartController', // inace je podeseno ng-controller atributom
+			url:'/sc',
+			templateUrl: 'partials/shoppingCart.html'
+		})
+		
+});
+
+webShop.config(function($logProvider){
+    $logProvider.debugEnabled(true);
 });
