8d10d8719d5be8bd4452bc1864b62b542bf6f4a2
divljiboy <ivan93.ns@hotmail.com>
2016-08-07 13:56:30 +0200

    hkjhk

---
+++ AngularWebShop/WebContent/app/appconfig.js
@@ -0,0 +1,21 @@
+(function(angular){
+
+    angular.module('webShop').config(['$stateProvider', '$urlRouterProvider', '$httpProvider', function ($stateProvider, $urlRouterProvider, $httpProvider) {
+
+        $urlRouterProvider.otherwise('/home');
+        $stateProvider
+          .state('home', {
+              url: '/home',
+              views: {
+                  'navbar': {
+                      templateUrl: 'partials/navbar.html'/*,
+                      controller: 'navbarController'*/
+                  }
+              }
+          });
+
+        
+    }]);
+
+  
+})(angular);
---
+++ AngularWebShop/WebContent/app/proizvoid/prozivodServicee.js
@@ -0,0 +1,38 @@
+(function () {
+
+    angular.module('webShop')
+        .service('prozivodService', ['$http', function ($http) {
+            //metode servisa
+            return {
+                getAll: function (onSuccess, onError) {
+                    
+                   $http.get('/AngularWebShop/rest/proizvodi/getJustProducts');
+                },
+                put: function (banka, onSucces, onError)
+                    {
+                
+                },
+                    post: function (banka, onSuccess, onError) {
+                    	$http.post('/AngularWebShop/rest/proizvodi/add', {"id":''+product.id, "count":parseInt(product.count)});
+                    	
+                    },
+                    deleteSlog: function (banka, onSuccess, onError) {
+                        var req = {
+                            method: 'DELETE',
+                            url: 'api/banka/' + banka.Id,
+                            
+                            headers: {
+                                'Content-Type': 'application/json'
+                            }
+                        };
+                        $http(req).then(onSuccess, onError);
+
+                    }
+            }
+
+
+
+
+        }]);
+
+})();
