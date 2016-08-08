3c50277f458621a55b3d02e288b0cd0e175e7bd5
kiljee <okiljevic93@gmail.com>
2016-08-08 16:24:45 +0200

    fsdf

--- AngularWebShop/WebContent/app/proizvoid/allProductsController.js
+++ AngularWebShop/WebContent/app/proizvoid/allProductsController.js
@@ -2,32 +2,49 @@
        
     angular.module('webShop')
     	.controller('allProductsController', ['$scope', '$state', '$rootScope', 'productsService', '$stateParams','uiGridConstants', function ($scope, $state, $rootScope, productsService,uiGridConstants, $stateParams) {
-		      
+    		$scope.gridOptions={};
     		productsService.getAll().then(function(response) {
 				   $scope.proizvodi = response.data;
 			       $scope.gridOptions.data = response.data;
 				    	           
 		    });
     		
-    		$scope.gridOptions={};
-		    $scope.gridOptions.multiSelect = false;
-		    $scope.gridOptions.modifierKeysToMultiSelect = false;
-		    $scope.gridOptions.noUnselect = true;
-		    		    
-		    $scope.gridOptions.onRegisterApi = function( gridApi ) {
-		    			  $scope.gridApi = gridApi;
-		    };
-		    			   
-		    $scope.toggleRowSelection = function() {
-		    	$scope.gridApi.selection.clearSelectedRows();
-		        $scope.gridOptions.enableRowSelection = !$scope.gridOptions.enableRowSelection;
-		        $scope.gridApi.core.notifyDataChange( uiGridConstants.dataChange.OPTIONS);
-		    	};
-		    	        
+    		
+    		 $scope.selektovaniSlog = {};
+    		 $scope.gridOptions.multiSelect = false;
+             $scope.gridOptions.noUnselect = true;
+             $scope.gridOptions.onRegisterApi = function (gridApi) {
+                 $scope.gridApi = gridApi;
+                 gridApi.selection.on.rowSelectionChanged($scope, function (row) {
+
+                     $scope.selektovaniSlog.Naziv = row.entity.Naziv;   //----
+                     $scope.selektovaniSlog.Id = row.entity.Id;          //----
+
+                     
+
+
+                 });
+
+             };
 		    	 
-		    
-		    	 
-		       $scope.obrisiSlog = function () {
+		    $scope.obrisiSlog = function () {
+		    	   if ($scope.gridApi.selection.getSelectedRows().length > 0) {
+	                    console.log("Ime drzave koja se brise : " + $scope.selektovaniSlog.Naziv);
+	                    
+	                    $scope.table = $scope.selektovaniSlog;
+	                    $scope.animationsEnabled = true;
+
+	                    
+	                    var modalInstance = $uibModal.open({
+	                        backdrop: false,
+	                        templateUrl: 'AngularClient/brisanje/dialogPotvrdeBrisanja.html',
+	                        controller: 'brisanjeSlogaController',
+	                        scope: $scope
+	                    });
+	                }
+	                else {
+	                    alert("Niste selektovali drzavu !");
+	                }
 		    	
 		       };
 		       
