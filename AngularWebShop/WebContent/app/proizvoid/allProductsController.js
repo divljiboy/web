262bcd1cb378a66a436c3e686bae09353965c3cf
kiljee <okiljevic93@gmail.com>
2016-08-08 17:03:33 +0200

    dada

--- AngularWebShop/WebContent/app/proizvoid/allProductsController.js
+++ AngularWebShop/WebContent/app/proizvoid/allProductsController.js
@@ -2,61 +2,24 @@
        
     angular.module('webShop')
     	.controller('allProductsController', ['$scope', '$state', '$rootScope', 'productsService', '$stateParams','uiGridConstants', function ($scope, $state, $rootScope, productsService,uiGridConstants, $stateParams) {
-    		$scope.gridOptions={};
-    		productsService.getAll().then(function(response) {
-				   $scope.proizvodi = response.data;
-			       $scope.gridOptions.data = response.data;
-				    	           
-		    });
-    		
-    		
-    		 $scope.selektovaniSlog = {};
-    		 $scope.gridOptions.multiSelect = false;
-             $scope.gridOptions.noUnselect = true;
-             $scope.gridOptions.onRegisterApi = function (gridApi) {
-                 $scope.gridApi = gridApi;
-                 gridApi.selection.on.rowSelectionChanged($scope, function (row) {
-
-                     $scope.selektovaniSlog.Naziv = row.entity.Naziv;   //----
-                     $scope.selektovaniSlog.Id = row.entity.Id;          //----
-
-                     
-
-
-                 });
-
-             };
-		    	 
-		    $scope.obrisiSlog = function () {
-		    	   if ($scope.gridApi.selection.getSelectedRows().length > 0) {
-	                    console.log("Ime drzave koja se brise : " + $scope.selektovaniSlog.Naziv);
-	                    
-	                    $scope.table = $scope.selektovaniSlog;
-	                    $scope.animationsEnabled = true;
-
-	                    
-	                    var modalInstance = $uibModal.open({
-	                        backdrop: false,
-	                        templateUrl: 'AngularClient/brisanje/dialogPotvrdeBrisanja.html',
-	                        controller: 'brisanjeSlogaController',
-	                        scope: $scope
-	                    });
-	                }
-	                else {
-	                    alert("Niste selektovali drzavu !");
-	                }
-		    	
-		       };
 		       
-		       $scope.dodajSlog = function () {
+    		 $scope.selektovaniSlog={};
+		       
 		    	 
-		    	   $state.go('addProducts')
-		   
-		       };
+		    		var podaci=function(){ productsService.getAll().then(function(response) {
+		    	        $scope.proizvodi = response.data;
+		    	        $scope.gridOptions.data = response.data;
+		    	    });
+		    		};
+		    	 
+podaci();
+		    	 
+		      
 		      
 	
-		       $scope.gridOptions = { enableRowSelection: true, enableRowHeaderSelection: false };
-	            	$scope.gridOptions.columnDefs = [
+		       $scope.gridOptions = {enableRowSelection: true, enableRowHeaderSelection: false };
+		    		   
+	           $scope.gridOptions.columnDefs = [
 	            	                                 { name: 'sifra' }, //----
 	            	                                 { name: 'naziv' },
 	            	                                 { name: 'boja' },
@@ -66,12 +29,55 @@
 	            	                                 { name: 'cena' },
 	            	                                 { name: 'kolicina' },
 	            	                                 { name: 'ocena' }
-	            	                                 ];
+	            	                            ];
 	            	
-		      
-    			}]);
-			    
-					    
-		   
-}		    
-)(angular);
+	           $scope.gridOptions.multiSelect = false;
+	            $scope.gridOptions.noUnselect = true;
+	            $scope.gridOptions.onRegisterApi = function (gridApi) {
+	                $scope.gridApi = gridApi;
+	                gridApi.selection.on.rowSelectionChanged($scope, function (row) {
+	                   
+	                    $scope.selektovaniSlog.sifra = row.entity.sifra;
+	                    $scope.selektovaniSlog.naziv = row.entity.naziv;
+	                    $scope.selektovaniSlog.boja = row.entity.boja;
+	                    $scope.selektovaniSlog.dimenzija = row.entity.dimenzija;
+	                    $scope.selektovaniSlog.tezina = row.entity.tezina;
+	                    $scope.selektovaniSlog.zemlja = row.entity.zemlja;
+	                    $scope.selektovaniSlog.cena = row.entity.cena;
+	                    $scope.selektovaniSlog.kolicina = row.entity.kolicina;
+	                    $scope.selektovaniSlog.ocena = row.entity.ocena;
+	                   
+
+	                });
+	            };
+	           
+	           
+	            $scope.obrisiSlog = function () {
+			    	
+	            	if ($scope.gridApi.selection.getSelectedRows().length > 0) {
+
+	                    console.log($scope.selektovaniSlog.sifra);
+	                   
+	                    $scope.animationsEnabled = true;
+
+	                    //ovo je moje nesto
+	                   /*
+	                        $state.go('UkidanjeRacuna', { 'idRacun': $scope.selektovaniSlog.Id });
+	                    else
+	                        alert("Racun " + $scope.selektovaniSlog.BrojRacuna + " je vec ukinut");
+	                   */
+	                }
+	                else {
+	                    alert("Niste selektovali nista !");
+	                }
+			       };
+			       
+			       $scope.dodajSlog = function () {
+			    	 
+			    	   $state.go('addProducts');
+			   
+			       };
+	           
+	           
+}]);		   
+})(angular);
