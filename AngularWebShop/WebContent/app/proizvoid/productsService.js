8d1dfe581da0717c0aaa72c4426be30b0e6f3932
divljiboy <ivan93.ns@hotmail.com>
2016-08-12 17:54:52 +0200

    asd

--- AngularWebShop/WebContent/app/proizvoid/productsService.js
+++ AngularWebShop/WebContent/app/proizvoid/productsService.js
@@ -15,7 +15,7 @@
                 
                 },
                 post: function (product) {
-                    	$http.post('/AngularWebShop/rest/product/add', product).success(function(data) {
+                    	$http.post('/AngularWebShop/rest/product/add', product).than(function(data) {
       		    		  
  		    			  return data;
                     });
