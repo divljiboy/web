2851623914d4727ecf6fc1d535fa0b21a2d26e63
Aleksandar Okiljević <okiljevic93@gmail.com>
2016-09-16 15:46:12 +0200

    fs

--- AngularWebShop/src/services/UserService.java
+++ AngularWebShop/src/services/UserService.java
@@ -17,12 +17,9 @@ import beans.Prodavnica;
 import beans.User;
 import beans.UserSer;
 
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.impl.crypto.MacProvider;
-import java.security.Key;
+
 import java.util.List;
-import java.util.Map;
+
 
 @Path("/user")
 public class UserService {
